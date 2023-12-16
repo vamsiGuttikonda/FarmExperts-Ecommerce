311b07d7ba63b7cd5365be8a7511e8f9b65e98f1
vamsiGuttikonda <vamsi.leetcode@gmail.com>
2023-12-16 16:50:19 +0530

    absjb

--- src/components/Data/productdata.js
+++ src/components/Data/productdata.js
@@ -2,12 +2,12 @@ export const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 1024 },
-      items: 5,
+      items: 4,
       slidesToSlide: 2,
     },
     desktop: {
       breakpoint: { max: 1024, min: 800 },
-      items: 4,
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 800, min: 464 },
@@ -24,7 +24,7 @@ export const responsive = {
     {
       id: 1,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "Brown Honey",
       price: "19.99",
       description: "Some text about the product about ",
@@ -34,7 +34,7 @@ export const responsive = {
     {
       id: 2,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "Red Honey",
       price: "21.99",
       description: "Some text about the product..",
@@ -44,7 +44,7 @@ export const responsive = {
     {
       id: 3,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "purple honey",
       price: "99.99",
       description: "Some text about the product..",
@@ -54,7 +54,7 @@ export const responsive = {
     {
       id: 4,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "black honey",
       price: "14.99",
       description: "Some text about the product..",
@@ -64,7 +64,7 @@ export const responsive = {
     {
       id: 5,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "white honey",
       price: "38.99",
       description: "Some text about the product..",
@@ -74,7 +74,7 @@ export const responsive = {
     {
       id: 6,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "green honey",
       price: "149.99",
       description: "Some text about the product..",
@@ -84,7 +84,7 @@ export const responsive = {
     {
       id: 7,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "east honey",
       price: "38.99",
       description: "Some text about the product..",
@@ -94,7 +94,7 @@ export const responsive = {
     {
       id: 8,
       imageurl:
-        "https://www.shutterstock.com/image-photo/ideas-gift-honey-packed-small-260nw-1637571250.jpg",
+        "https://images.unsplash.com/photo-1587049352851-8d4e89133924?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
       name: "blue honey",
       price: "49.99",
       description: "Some text about the product..",
--- src/components/product-slider/courosel.js
+++ src/components/product-slider/courosel.js
@@ -3,17 +3,19 @@ import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import CouroselIndividual from './couroselindividual';
+import Product_Card from '../Product/Product_Card';
 import { responsive, productData } from '../Data/productdata';
 
 const ProductSlider = (props) => {
         // the above props is for heading 
   const product = productData.map((item) => (
-    <CouroselIndividual
-      name={item.name}
-      imageurl={item.imageurl}
-      price={item.price}
-      description={item.description}
-    />
+    // <CouroselIndividual
+    //   name={item.name}
+    //   imageurl={item.imageurl}
+    //   price={item.price}
+    //   description={item.description}
+    // />
+    <Product_Card product={item}/>
   ));
 
   return (
